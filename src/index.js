ce28eb953cba8e1848cabd96107ac3afc8c51bc1
“Aaron <github email address>
2021-03-03 11:23:07 -0500

    almost done with 2nd deliverable

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ const title = document.querySelector("h2.title")
 const list = document.querySelector("ul.ingredients-list")
 const blendsURL = "http://localhost:3000/spiceblends"
 
+const updateForm = document.querySelector("form#update-form")
+const ingredientForm = document.querySelector("form#ingredient-form")
+
 fetch(`${blendsURL}/1`)
     .then(response => response.json())
     .then(blend => getBlendDetail(blend))
@@ -20,4 +23,25 @@ function getBlendDetail(blend){
         li.textContent = ingredient.name
         list.append(li)
     })
+    updateForm.dataset.id = blend.id
 }
+
+updateForm.addEventListener('submit', event => {
+    event.preventDefault()
+    
+    const title = event.target.title.value
+
+    fetch(`${blendsURL}/${updateForm.dataset.id}`, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type':'application/json',
+            'Accept':'application/json'
+    
+        },
+        body: JSON.stringify({title})
+    })
+    .then(response => response.json())
+    .then(patchedBlend => title.textContent = patchedBlend.title)
+    updateForm.reset()
+})
+    
