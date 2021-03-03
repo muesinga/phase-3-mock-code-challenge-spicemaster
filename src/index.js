4e0940f03b2bef02ef36f41038597a033195ad60
“Aaron <github email address>
2021-03-03 10:58:01 -0500

    first deliverable completed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-// write your code here
+const detail = document.querySelector("div#spice-blend-details")
+const image = document.querySelector("img.detail-image")
+const title = document.querySelector("h2.title")
+const list = document.querySelector("ul.ingredients-list")
+const blendsURL = "http://localhost:3000/spiceblends"
+
+fetch(`${blendsURL}/1`)
+    .then(response => response.json())
+    .then(blend => getBlendDetail(blend))
+
+function getBlendDetail(blend){
+    
+    image.src = blend.image
+    image.alt = blend.title
+
+    title.textContent = blend.title
+
+    blend.ingredients.forEach(ingredient => {
+        const li = document.createElement('li')
+        li.textContent = ingredient.name
+        list.append(li)
+    })
+}
